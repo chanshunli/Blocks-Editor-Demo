90bb88c8b1775469f5ccd679cb7bf6e75994dfd8
Steve Chan <chanshunli@gmail.com>
2019-03-19 13:09:54 +0800

    在linux运行build-client-mac.sh,在Mac上装=>OK

--- build-client-mac.sh
+++ build-client-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-lein cljsbuild once release
+# lein cljsbuild once release # 不能用这个命令,不然 ./app/js/out/goog/base.js 文件就找不到了,html的 => 要用figwheel就好
 app/node_modules/.bin/electron-packager app blocks-editor-demo --platform=darwin --asar=true --electronVersion=1.7.10
 
